7e5e749e6b0477ae7ee124a05536710d8051c89d
henrikborder7 <henrik.nese@gmail.com>
2024-01-26 14:28:10 +0100

    Update +page.svelte

--- prosjektoppgave/src/routes/nyheter/+page.svelte
+++ prosjektoppgave/src/routes/nyheter/+page.svelte
@@ -1,22 +1,26 @@
 <script>
-      import fil from "../postnummer (1).json"
-    
-    
+    import fil from "../postnummer (1).json"
+  
     let json = JSON.parse(JSON.stringify(fil))
     let postnummer = ""
   
-        
-    function hentPoststed(postnummer) {
-    for (let i = 0; i < json.length; i++) {
-    if(json[i].Postnummer === postnummer)
-    return json[i].Poststed
-                 }
-                 return ""
-           }
-</script>
-{#if fil}
-<h3>Postnummer</h3>
-<input placeholder="Postnummer" bind:value={postnummer}/>
-<h3>Poststed</h3>
-{hentPoststed(postnummer)}
-{/if}
+    function hentPoststed() {
+      for (let i = 0; i < json.length; i++) {
+        if (json[i].Postnummer === postnummer) {
+          return json[i].Poststed
+        }
+      }
+      return ""
+    }
+  </script>
+  
+  <h1>Meld deg på vårt nyhetsbrev</h1>
+  <form>
+    {#if fil}
+      <h3>Postnummer</h3>
+      <input placeholder="Postnummer" bind:value={postnummer} required/>
+      <h3>Poststed</h3>
+      {hentPoststed()}
+    {/if}
+    <button>Meld på</button>
+  </form>
