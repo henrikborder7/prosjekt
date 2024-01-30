4424ca668ddfbffb4dff2b5a13dab765cbc8fe33
Crizzyborder30 <126252757+Crizzyborder30@users.noreply.github.com>
2024-01-30 09:39:58 +0100

    Update +page.svelte

--- prosjektoppgave/src/routes/tabell/+page.svelte
+++ prosjektoppgave/src/routes/tabell/+page.svelte
@@ -3,6 +3,7 @@
     let json = JSON.parse(JSON.stringify(fil));
 
     let navn = "";
+    let navner = []
 
 
     function sokEtterSpiller(navn) {
@@ -14,17 +15,6 @@
         return "";
     }
 
-    function skEtterSpiller(navn) {
-        let i = 0
-        while(i < json.elements.length) {
-            if (json.elements[i].first_name === navn) {
-                return json.elements[i].first_name+" "+ json.elements[i].second_name;
-            }
-            i = i++
-        }
-        return "";
-    }
-
     json.elements.sort(function(a,b){
         return b.goals_scored - a.goals_scored
     });
@@ -32,14 +22,39 @@
     let rangertListe = json.elements.map(function(spiller){
         return spiller.first_name +" "+spiller.second_name+ ": "+ spiller.goals_scored +" mål"
     }) 
-    console.log(rangertListe)
+
+
+    
+    function skEtterSpiller(fornavn){let matchendeSpillere= []
+
+    for (let i = 0; i < json.elements.length; i++) {
+        if (json.elements[i].first_name === fornavn) {
+        matchendeSpillere.push(json.elements[i].first_name + " " + json.elements[i].second_name);
+    }
+  }
+visSpillere(matchendeSpillere)
+    }
+
+    function visSpillere(spillere) {
+  if (spillere.length === 0) {
+    return "Ingen spillere funnet.";
+  } else {
+    for (let i = 0; i < spillere.length; i++) {
+      return spillere[i]
+    }
+  }
+    }
+
 </script>
 
 <label><input bind:value={navn}>Antal mål
 </label>
 
+<p>{navner}</p>
+
 <p>{sokEtterSpiller(navn)}</p>
-<p>{skEtterSpiller(navn)}</p>
+<p>{visSpillere(navn)}</p>
+
 <p>{json.elements[0].first_name}</p>
 
 <ul>
