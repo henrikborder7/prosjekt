6784a50a4a94e90870cb0499352fca3160f64524
henrikborder7 <henrik.nese@gmail.com>
2024-02-08 10:15:12 +0100

    Update +page.svelte

--- prosjektoppgave/src/routes/butikk/+page.svelte
+++ prosjektoppgave/src/routes/butikk/+page.svelte
@@ -9,7 +9,15 @@
         { id: 3, name: "Mini PL-trofee", pris: 199, beskrivelse: "Et mini trofee, perfekt for hylla! Høyde 16cm.", bilde: "https://img.fruugo.com/product/3/13/1267893133_max.jpg" },
         { id: 4, name: "Offisiell ball 23/24", pris: 1499, beskrivelse: "Årets ofisiell kampball. Ypperste kvalitet, og med teknologi som gjør at ballen suser inn i nettmaskene", bilde: "https://www.torshovsport.no/pub_images/original/79718.jpg" },
         { id: 5, name: "Nike leggbeskyttere", pris: 479, beskrivelse: "Robuste leggbeskyttere med leggskinn holder. Material: syntetisk gjenvunnet plastikk.", bilde: "https://lovellcdn.b-cdn.net/products/242469.jpg" },
-        { id: 6, name: "Manchester United FC ", pris: 0, beskrivelse: "Rotten klubb gis bort.", bilde: "https://yt3.googleusercontent.com/TDwtV-eV1UitynrmZjOnnlFCsNeoNYG3z15fDsr4P8xJkSUqWhFYC6a77namw4P0TiYS2No7=s900-c-k-c0x00ffffff-no-rj" }
+        { id: 6, name: "Manchester United FC ", pris: 0, beskrivelse: "Rotten klubb gis bort.", bilde: "https://yt3.googleusercontent.com/TDwtV-eV1UitynrmZjOnnlFCsNeoNYG3z15fDsr4P8xJkSUqWhFYC6a77namw4P0TiYS2No7=s900-c-k-c0x00ffffff-no-rj" },
+        { id: 7, name: "Nike leggbeskyttere", pris: 479, beskrivelse: "Robuste leggbeskyttere med leggskinn holder. Material: syntetisk gjenvunnet plastikk.", bilde: "https://lovellcdn.b-cdn.net/products/242469.jpg" },
+        { id: 8, name: "Nike leggbeskyttere", pris: 479, beskrivelse: "Robuste leggbeskyttere med leggskinn holder. Material: syntetisk gjenvunnet plastikk.", bilde: "https://lovellcdn.b-cdn.net/products/242469.jpg" },
+        { id: 9, name: "Nike leggbeskyttere", pris: 479, beskrivelse: "Robuste leggbeskyttere med leggskinn holder. Material: syntetisk gjenvunnet plastikk.", bilde: "https://lovellcdn.b-cdn.net/products/242469.jpg" },
+        { id: 10, name: "Nike leggbeskyttere", pris: 479, beskrivelse: "Robuste leggbeskyttere med leggskinn holder. Material: syntetisk gjenvunnet plastikk.", bilde: "https://lovellcdn.b-cdn.net/products/242469.jpg" },
+        { id: 11, name: "Nike leggbeskyttere", pris: 479, beskrivelse: "Robuste leggbeskyttere med leggskinn holder. Material: syntetisk gjenvunnet plastikk.", bilde: "https://lovellcdn.b-cdn.net/products/242469.jpg" },
+        { id: 12, name: "Nike leggbeskyttere", pris: 479, beskrivelse: "Robuste leggbeskyttere med leggskinn holder. Material: syntetisk gjenvunnet plastikk.", bilde: "https://lovellcdn.b-cdn.net/products/242469.jpg" }
+       
+
     ];
 
     function addProductToCart(product) {
@@ -50,6 +58,7 @@
 
     function updateCart() {
         cart = cartItems.reduce((total, item) => total + item.quantity, 0);
+        console.log("cart:", cart, "Cartitems:", cartItems)
     }
 </script>
 
@@ -80,6 +89,7 @@
     </nav>
 
     <main>
+        
         {#each produkt as product (product.id)}
         {#if product.id<9}
             <div class="product">
@@ -89,8 +99,9 @@
                 <p>Pris: {product.pris},-</p>
                 <button on:click={() => addProductToCart(product)}>Legg til i handlevogna</button>
             </div>
+            
             {:else}
-            <button>Last inn fler</button>
+            <button class="lastInnFler">Last inn fler</button>
             {/if}
         {/each}
     </main>
@@ -151,11 +162,18 @@
     <p>&copy; 2024 PL-butikken. Alle rettigheter reservert.</p>
 </footer>
 <style>
+    .lastInnFler{
+        background-color: greenyellow ;
+        align-content: center;
+        justify-items: center;
+        padding:20px;
+        border-radius:14px
+    }
      footer {
         background-color: #333;
         color: white;
         text-align: center;
-        padding: 10px;
+        padding: 10px 0px 10px 0px;
         
         bottom: 0;
         width: 100%;
@@ -214,9 +232,9 @@
 
     main {
         padding: 20px;
-        display: grid;
-        gap: 20px;
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+      display: grid;
+      gap: 20px;
+      grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
         justify-content: flex-end; /* Align products to the right */
     }
     .product {
@@ -227,6 +245,11 @@
         padding: 20px;
         margin-bottom: 20px;
         max-width: 350px;
+       
+       
+        gap: 20px;
+       
+        justify-content: flex-end; /* Align products to the right */
     }
 
     button {
